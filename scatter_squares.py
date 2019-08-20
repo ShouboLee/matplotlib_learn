7e840692ffdfcbab1abe39cb6eb78cd480c096ab
lsb <lishoubo2014@126.com>
2019-08-20 16:14:10 +0800

    chap 15.2 add scatter_squares.py

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolor='none', s=40)
 
 #设置图表标题并给坐标轴加上标签
 plt.title('Square Numbers', fontsize=24)
@@ -10,4 +13,6 @@ plt.ylabel('Square of Value', fontsize=14)
 #设置刻度标记大小
 plt.tick_params(axis='both', which='major', labelsize=14)
 
+#打印到文件输出
+#plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
